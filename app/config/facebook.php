e79b5a7e2781a4bdcf28c3a28d37f3d38d065dc9
Paolo <sl.paolo.paz@gmail.com>
2016-02-10 16:58:22 -0500

    subiendo Proyecto Aouth

---
+++ app/config/facebook.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return array(
+  'app_id'=>'472493926284408',
+  'app_secret'=>'d09c7c53b467185b563687266d7ffe6c',
+  'app_scope'=> array('user_birthday','email')
+);
---
+++ app/controllers/LoginFacebookController.php
@@ -0,0 +1,18 @@
+<?php
+
+class LoginFacebookController extends \BaseController {
+
+	private $fb;
+	public function __construct(FacebookHelper $fb){
+		$this->fb=$fb;
+
+	}
+	public function login(){
+
+	}
+	public function callback(){
+
+	}
+
+
+}
